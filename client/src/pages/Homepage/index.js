ab41ff5ff9a50102a8ccde99f509c0b8cc562ada
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-08-14 14:32:27 -0400

    another try

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -25,10 +25,12 @@ export default class Homepage extends Component {
     }
 
     componentDidMount() {
-        console.log("here!")
-        let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        window.addEventListener('resize', () => {
+            console.log("resizing")
+            // We execute the same script as before
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+          });
     }
 
 
