cf6497420fddf321e1d1f3f7887a6009892659bb
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-08-14 17:33:55 -0400

    cleaned up

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -4,10 +4,10 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import HomeHero from "../../components/HomeHero/index"
 import './style.css';
-import profPic from "../../media/5.PNG"
+// import profPic from "../../media/5.PNG"
 import logo from "../../media/Blue-Heron-White.png"
 import heroImg from "../../media/sand-beach.png"
-import arrow from "../../media/arrow.png"
+// import arrow from "../../media/arrow.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft, faGlassMartini } from '@fortawesome/free-solid-svg-icons'
 
