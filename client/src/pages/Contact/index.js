a6970734827761d9465771ab01b89d4f3a368e15
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-08-22 07:35:49 -0400

    nodemailer added

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,4 +1,4 @@
-import { Container, Nav, Button, Image, Row, Col } from 'react-bootstrap'
+import { Container, Nav, Button, Image, Row, Col, Spinner, Alert } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
 import React, { Component } from 'react';
 // import logo from './logo.svg';
@@ -20,18 +20,83 @@ export default class Contact extends Component {
 
         this.listener = null;
         this.state = {
-            status: "top"
+            status: "top",
+            showModal: false,
+            emailSent: false,
+            emailSending: false,
+            errorMsg: false
         };
+        this.name = React.createRef();
+        this.email = React.createRef();
+        this.subject = React.createRef();
+        this.message = React.createRef();
     }
 
 
+    openModal = () => {
+        this.setState({
+            showModal: true
+        })
+    }
+
+    sendEmail = (e) => {
+        e.preventDefault();
+
+        this.setState({
+            emailSending: true
+        })
+        let name = this.name.current.value
+        let email = this.email.current.value
+        let subject = this.subject.current.value
+        let message = this.message.current.value
+
+        const postItem = () => {
+            console.log("posting to DB")
+            // POST TO DB
+            fetch('/api/sendmail', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    subject: subject,
+                    message: message
+                })
+            }).then(response => {
+                console.log("email sent")
+                console.log(response)
+                if (response.status == '200') {
+                    this.setState({
+                        // showModal: false,
+                        emailSending: false,
+                        emailSent: true
+                    })
+                }
+                // else if (response.status == '500') {
+                //     alert("500 error")
+                // }
+                else {
+                    this.setState({
+                        errorMsg: true,
+                        emailSending:false
+                    })
+                }
+            })
+
+        }
+        postItem()
+    }
+
     componentDidMount() {
-       const heightNum = window.innerHeight;
+        const heightNum = window.innerHeight;
         let height = window.innerHeight + 'px';
         let topHeight = heightNum * 0.56 + 'px';
         let bottomHeight = heightNum * 0.44 + 'px';
         console.log(bottomHeight)
-  
+
 
 
         this.setState({
@@ -51,8 +116,8 @@ export default class Contact extends Component {
                 topHeight: topHeight,
                 bottomHeight: bottomHeight
             })
-     
-          });
+
+        });
         // this.listener = document.addEventListener("scroll", e => {
         //     var scrolled = document.scrollingElement.scrollTop;
         //     if (scrolled >= 120) {
@@ -73,8 +138,7 @@ export default class Contact extends Component {
 
     render() {
 
-        const {topHeight, bottomHeight} = this.state;
-
+        const { topHeight, bottomHeight, showModal, emailSending, emailSent, errorMsg } = this.state;
 
 
 
@@ -85,10 +149,8 @@ export default class Contact extends Component {
 
 
                 <Row>
-                    {/* <Col md={3}></Col> */}
-
                     <Col sm={12} md={12} className="home-img" style={{
-                        // backgroundImage: `url(${heroImg})`,
+              
                         backgroundColor: `#0E3B62`,
                         opacity: `90%`,
                         backgroundBlendMode: `multiply`,
@@ -100,15 +162,12 @@ export default class Contact extends Component {
                         display: `flex`,
                         alignItems: `center`,
                         position: `relative`
-                        //   cover no-repeat center center fixed`
                     }}>
                         <a href="/"><Image className="hero-logo-contact" src={logo}></Image></a>
                     </Col>
-                    {/* <Col md={3}></Col> */}
+              
                 </Row>
                 <Row>
-                    {/* <Col md={3}></Col> */}
-
                     <Col sm={12} md={12} className="home-img" style={{
                         backgroundImage: `url(${heroImg})`,
                         backgroundColor: `#F9F8F0`,
@@ -117,21 +176,63 @@ export default class Contact extends Component {
                         backgroundPosition: `center`,
                         backgroundSize: `cover`,
                         backgroundRepeat: `no-repeat`,
-                        // backgroundAttachment: `fixed`,
                         height: `${bottomHeight}`,
                         display: `flex`,
                         alignItems: `center`,
                         position: `relative`
-                        //   cover no-repeat center center fixed`
                     }}>
                         <div className="contact-box">
                             <h1>Contact us</h1>
                             <p>Drop us a line the next time you're in Raleigh</p>
-                            <a href="mailto:info@blueheronculinary.com?subject=Contact Blue Heron" subect="Contact Blue Heron"><button className="contact-btn-link">Email us</button></a>
+                            {/* <a href="mailto:info@blueheronculinary.com?subject=Contact Blue Heron"> */}
+                            <button onClick={this.openModal} className="contact-btn-link">Email us</button>
+                      
                         </div>
                     </Col>
-                    {/* <Col md={3}></Col> */}
+            
                 </Row>
+                {showModal ? (
+                    <div className="modal-canvas">
+                        <div className="contact-modal">
+                            <button className="exit-btn" onClick={() => { this.setState({ showModal: false }) }}>close</button>
+                            <div className="modal-content">
+                                <h1>Contact us</h1>
+                                <p>Plug in an email and send our way!</p>
+                                <form>
+                               
+                                    <input ref={this.name} placeholder="name" id="name"></input>
+                                
+                                    <input ref={this.email} placeholder="email" id="email"></input>
+                             
+                                    <input ref={this.subject} placeholder="subject" id="subject"></input>
+                              
+                                    <textarea ref={this.message} placeholder="message" id="message"></textarea>
+                                    {emailSending ? ( 
+                                        <Spinner animation="border" role="status">
+                                        <span className="sr-only">Loading...</span>
+                                      </Spinner>
+                                    ): (<button className="send-email-btn" onClick={this.sendEmail} type="submit">Send</button>)}
+                                  
+                                </form>
+                                <div>
+                                    {/* <br></br> */}
+                                    {emailSent ? (
+                                        <p>Your message has been sent</p>
+                                    ): (<div></div>)
+                                    }
+                                    {errorMsg ? (
+                                        <Alert variant='danger'>
+                                        There was trouble accessing email server. Email us directly
+                                        at info@blueheronculinary.com
+                                      </Alert>
+                                    ): (<div></div>)}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ) :
+                    (<div></div>)
+                }
             </div>
         )
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@
     const app = express();
     const port = process.env.PORT || 5000;
     
-    
+    const nodemailer = require("nodemailer");
     const morgan = require('morgan');
     const router = require("express").Router();
     const path = require("path");
@@ -32,7 +32,109 @@
     app.use(bodyParser.urlencoded({ extended: true }));
     
     
+    app.post('/api/sendmail', function (req, res) {
+      console.log("keys")
+      const data = {
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.message
+      };
+    
+  console.log(data);
+
+let mailTo = `mailto:${data.email}?subject=RE:${data.subject}`;
+let emailContent = `
+
+<style>
+p {
+  font-family: serif;
+  font-size:19px;
+}
+button {
+  font-family:serif;
+  display: inline-block;
+  font-weight: 400;
+  color: #0E3B62;
+  text-align: center;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid #0E3B62;
+  padding: .375rem .75rem;
+  font-size: 1.5rem;
+  line-height: 1.5;
+  border-radius: .25rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+
+button:hover {
+  color: #fff;
+  background-color: #6F8EA8;
+  border-color: #6F8EA8;
+}
+</style>
+<p><strong>${data.name}</strong> has contacted you:<p>
+ <p><a href=${mailTo}>Email: ${data.email}<a><p>
+ <p>Subject: ${data.subject}<p>
+ <hr>
+ <p>Message: </p>
+ <p>${data.message}</p>
+ <a href=${mailTo}><button>Reply</button></a>`
+
+  // async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'Godaddy',
+    host: process.env.host,
+    // host: 'smtpout.secureserver.net'
+    port: 587,
+    secureConnection: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.user, // generated ethereal user
+      pass: process.env.pass, // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized: false
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `info@blueheronculinary.com`, // sender address
+    to: "info@blueheronculinary.com", // list of receivers
+    subject: `${data.subject}`, // Subject line
+    // text: `${data.message}`, // plain text body
+    html: emailContent, // html body
+  });
+
   
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+if (info.messageId) {
+  res.send('POST request to the homepage')
+} else {
+  console.log("ERROR!")
+    return res.status(400).send({
+      message: 'This is an error!'
+    });
+}
+}
+
+main().catch((error) => {
+  if (error) {
+    console.log(error)
+    res.status(400).send();
+  }
+
+});
+    })
     
  
       if (process.env.NODE_ENV === 'production') {
