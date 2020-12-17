5c7e89e51732aebb0158caf5cd3661892554296a
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-12-17 17:04:33 -0500

    nav bar logo colors

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./style.css";
 // import Mobile from "./mobile"
-import logo from "../../media/logo.png"
-import fbLogo from "../../media/fb-grey.png"
-import instaLogo from "../../media/insta-grey.png"
-import cartLogo from "../../media/cart-grey.png"
+import logo from "../../media/logo.png";
+import fbLogo from "../../media/fb-grey.png";
+import instaLogo from "../../media/insta-grey.png";
+import cartLogo from "../../media/cart-grey.png";
+
+import fbLogoW from "../../media/fb-white.png";
+import instaLogoW from "../../media/insta-white.png";
+import cartLogoW from "../../media/cart-white.png";
 var _ = require("lodash");
 
 export default class Nav extends Component {
@@ -14,23 +18,30 @@ export default class Nav extends Component {
     this.listener = null;
     this.state = {
       status: "top",
-      mobileNavToggle: false
+      mobileNavToggle: false,
+      // isMobile: false
     };
   }
 
   toggleNav = () => {
-   if (this.state.mobileNavToggle) {
-     this.setState({
-         mobileNavToggle: false
-     })
-   } else if (!this.state.mobileNavToggle) {
-    this.setState({
-      mobileNavToggle: true
-  })
-   }
-  }
+    if (this.state.mobileNavToggle) {
+      this.setState({
+        mobileNavToggle: false,
+      });
+    } else if (!this.state.mobileNavToggle) {
+      this.setState({
+        mobileNavToggle: true,
+      });
+    }
+  };
 
-  componentDidMount() {}
+  componentDidMount() {
+    if (window.innerWidth > 750) {
+      this.setState({
+        isMobile: true,
+      });
+    }
+  }
 
   componentDidUpdate() {
     window.addEventListener(
@@ -41,14 +52,12 @@ export default class Nav extends Component {
             mobileNavToggle: false,
           });
           document.getElementById("navvy-bar").className = "header";
-          document.getElementById("menu-toggle").checked = false
-        } 
-        else if (window.innerWidth < 750) {
+          document.getElementById("menu-toggle").checked = false;
+        } else if (window.innerWidth < 750) {
           if (this.state.mobileNavToggle) {
             document.getElementById("menu-toggle").checked = true;
           }
         }
-          
       }, 400)
     );
   }
@@ -56,7 +65,10 @@ export default class Nav extends Component {
   render() {
     return (
       <div className="nav-section">
-      <header id="navvy-bar" class={this.state.mobileNavToggle ? 'mobile-header' : 'header'}>
+        <header
+          id="navvy-bar"
+          class={this.state.mobileNavToggle ? "mobile-header" : "header"}
+        >
           <nav className="nav-options">
             <ul>
               <li>
@@ -73,16 +85,44 @@ export default class Nav extends Component {
               </li>
             </ul>
           </nav>
+          {/* <div className="mobile-nav-logos">
+          <a  href="/"><img src={instaLogo}></img></a>
+          <a href="/"><img src={fbLogo}></img></a>
+             <a  href="/"><img src={cartLogo}></img></a>
+          </div> */}
           <div className="nav-brand">
             <img src={logo}></img>
           </div>
-          <div className="nav-right">
-             <a  href="/"><img src={instaLogo}></img></a>
-          <a href="/"><img src={fbLogo}></img></a>
-             <a  href="/"><img src={cartLogo}></img></a>
-          </div>
+          { this.state.isMobile?  <div className="nav-right">
+            <a href="/">
+              <img src={instaLogo}></img>
+            </a>
+            <a href="/">
+              <img src={fbLogo}></img>
+            </a>
+            <a href="/">
+              <img src={cartLogo}></img>
+            </a>
+          </div> : 
+           <div className="nav-right">
+           <a href="/">
+             <img src={instaLogoW}></img>
+           </a>
+           <a href="/">
+             <img src={fbLogoW}></img>
+           </a>
+           <a href="/">
+             <img src={cartLogoW}></img>
+           </a>
+         </div> }
+         
         </header>
-        <input type="checkbox" class="menu-toggle" id="menu-toggle" onClick={this.toggleNav} />
+        <input
+          type="checkbox"
+          class="menu-toggle"
+          id="menu-toggle"
+          onClick={this.toggleNav}
+        />
         <div class="mobile-bar">
           <label for="menu-toggle" class="menu-icon">
             <span></span>
