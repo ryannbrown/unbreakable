77ea46b1dfc29703c784963915eaf9d77015d479
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-12-18 13:01:25 -0500

    hero typing, small other style changes

--- client/src/components/homepage/HomeBlueBlock/index.js
+++ client/src/components/homepage/HomeBlueBlock/index.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import './style.css';
 // import profPic from "../../media/5.PNG"
 import waveImg from "../../../media/wave-img.jpg"
-
+import favWhite from "../../../media/fav-white.png"
 
 
 
@@ -49,19 +49,17 @@ export default class HomeBlueBlock extends Component {
             height: `100vh`,
             width:'100%',
             display: `flex`,
+            flexDirection: 'column',
             alignItems: `center`,
             justifyContent: 'center',
             position: `relative`
           }}>
             {/* <img className="hero-logo" src={placeHolder}></img> */}
+            <img src={favWhite}></img>
               <div className="bb-text-content">
-                <h1>UNBREAKABLE</h1>
+                <h1>You are not alone.</h1>
                
-                <p>Breaking the Silence.</p>
-                <div className="vert-line"></div>
-                <p>Finding My Voice.</p>
-                <div className="vert-line"></div>
-                <p>Sharing My Story.</p>
+                <p>Almost 1 in 5 adults suffer from a mental illness. In 2017, roughly 46.6 million people were diagnosed with mental health issues in the United States alone. If you or someone you know struggles with mental health, know that you are not alone.</p>
               </div>
             {/* <p style={{color:'#0e3b62'}} className="action-button">February 2021</p> */}
           </div>
--- client/src/components/homepage/HomeHero/index.js
+++ client/src/components/homepage/HomeHero/index.js
@@ -1,28 +1,34 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
-
-import './style.css';
+import "./style.css";
 // import profPic from "../../media/5.PNG"
 // import placeHolder from "../../../media/placeholder.png"
-import bookImg from "../../../media/book-img.png"
-
-
-
+import bookImg from "../../../media/book-img.png";
+import Typed from "typed.js";
 
 export default class Hero extends Component {
-
   constructor(props) {
     super(props);
 
     this.listener = null;
     this.state = {
-      status: "top"
+      status: "top",
+      // typed: ''
     };
   }
 
-
   componentDidMount() {
- 
+    var typed3 = new Typed(".typewriter", {
+      strings: [
+        "Breaking the Silence.",
+        "Finding My Voice.",
+        "Sharing My Story.",
+      ],
+      typeSpeed: 100,
+      backSpeed: 0,
+      smartBackspace: true, // this is a default
+      loop: true,
+    });
   }
 
   componentDidUpdate() {
@@ -30,36 +36,47 @@ export default class Hero extends Component {
   }
 
   render() {
-
-
-
-
-
     return (
-      <div className="home-img" style={{
-        // backgroundImage: `url(${heroImg})`,
-        backgroundColor: `#f9fafb`,
-        opacity: `100%`,
-        backgroundBlendMode: `multiply`,
-        backgroundPosition: `center`,
-        backgroundSize: `cover`,
-        backgroundRepeat: `no-repeat`,
-        backgroundAttachment: `fixed`,
-        // height: `${this.props.height}`,
-        height: `100vh`,
-        width:'100%',
-        display: `flex`,
-        alignItems: `center`,
-        justifyContent: 'center',
-        position: `relative`
-      }}>
-        <img className="hero-logo" src={bookImg}></img>
-   
+      <div
+        className="home-img"
+        style={{
+          // backgroundImage: `url(${heroImg})`,
+          backgroundColor: `#f9fafb`,
+          opacity: `100%`,
+          backgroundBlendMode: `multiply`,
+          backgroundPosition: `center`,
+          backgroundSize: `cover`,
+          backgroundRepeat: `no-repeat`,
+          backgroundAttachment: `fixed`,
+          // height: `${this.props.height}`,
+          height: `100vh`,
+          width: "100%",
+          display: `flex`,
+          alignItems: `center`,
+          justifyContent: "center",
+          position: `relative`,
+        }}
+      >
+        {/* <div className="typewriter"></div> */}
+        <div className="hero-content-section">
+          <div className="hero-text-block">
+            <div>
+              <h3>UNBREAKABLE</h3>
+            </div>
+            <div className="typewriter-section">
+            <div className="typewriter"></div>
+            </div>
 
-        <br></br>
+          </div>
+
+          <div className="hero-book-block">
+            <img className="hero-book-img" src={bookImg}></img>
+          </div>
+        </div>
+
+        {/* <br></br> */}
         {/* <p style={{color:'#0e3b62'}} className="action-button">February 2021</p> */}
       </div>
-    )
-
+    );
   }
 }
