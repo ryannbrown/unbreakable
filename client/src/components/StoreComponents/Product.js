bdae191e0549c3364840f99de898aab9a2591dea
timsteeleallen <timsteeleallen@gmail.com>
2020-12-28 19:14:13 -0500

    add text

--- client/src/components/StoreComponents/Product.js
+++ client/src/components/StoreComponents/Product.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Link} from "react-router-dom"
-import VariantSelector from './VariantSelector';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import VariantSelector from "./VariantSelector";
 
 class Product extends Component {
   constructor(props) {
@@ -11,9 +11,9 @@ class Product extends Component {
       defaultOptionValues[selector.name] = selector.values[0].value;
     });
     this.state = {
-       selectedOptions: defaultOptionValues,
-        selectedVariantQuantity: 0
-     };
+      selectedOptions: defaultOptionValues,
+      selectedVariantQuantity: 0,
+    };
 
     this.handleOptionChange = this.handleOptionChange.bind(this);
     this.handleQuantityChange = this.handleQuantityChange.bind(this);
@@ -31,29 +31,32 @@ class Product extends Component {
   }
 
   handleOptionChange(event) {
-    const target = event.target
+    const target = event.target;
     let selectedOptions = this.state.selectedOptions;
     selectedOptions[target.name] = target.value;
 
-    const selectedVariant = this.props.client.product.helpers.variantForOptions(this.props.product, selectedOptions)
+    const selectedVariant = this.props.client.product.helpers.variantForOptions(
+      this.props.product,
+      selectedOptions
+    );
 
     this.setState({
       selectedVariant: selectedVariant,
-      selectedVariantImage: selectedVariant.attrs.image
+      selectedVariantImage: selectedVariant.attrs.image,
     });
   }
 
   quantChange(action, id, quant) {
-    console.log(action)
-    if (action === '+') {
+    console.log(action);
+    if (action === "+") {
       this.setState({
-        selectedVariantQuantity: this.state.selectedVariantQuantity + 1
-      })
+        selectedVariantQuantity: this.state.selectedVariantQuantity + 1,
+      });
       // this.props.addVariantToCart(id, quant)
-    } else if (action ==='-' && this.state.selectedVariantQuantity >= 1) {
+    } else if (action === "-" && this.state.selectedVariantQuantity >= 1) {
       this.setState({
-        selectedVariantQuantity: this.state.selectedVariantQuantity - 1
-      })
+        selectedVariantQuantity: this.state.selectedVariantQuantity - 1,
+      });
     }
   }
 
@@ -61,17 +64,17 @@ class Product extends Component {
     console.log(event);
     if (event.target.value >= 0) {
       this.setState({
-        selectedVariantQuantity: event.target.value
+        selectedVariantQuantity: event.target.value,
       });
     }
   }
 
-
   render() {
     // console.log(this.props.product);
     // console.log( this.state.selectedVariantQuantity)
-    let variantImage = this.state.selectedVariantImage || this.props.product.images[0]
-    let variant = this.state.selectedVariant || this.props.product.variants[0]
+    let variantImage =
+      this.state.selectedVariantImage || this.props.product.images[0];
+    let variant = this.state.selectedVariant || this.props.product.variants[0];
     let variantQuantity = this.state.selectedVariantQuantity || 0;
     let variantSelectors = this.props.product.options.map((option) => {
       return (
@@ -85,33 +88,54 @@ class Product extends Component {
     return (
       <div className="Product">
         <div className="prod-left">
-        {/* <Link to={`/shop/item/${this.props.product.handle}`}> */}
-        {this.props.product.images.length ? <img src={variantImage.src} alt={`${this.props.product.title} product shot`}/> : null}
+          {/* <Link to={`/shop/item/${this.props.product.handle}`}> */}
+          {this.props.product.images.length ? (
+            <img
+              src={variantImage.src}
+              alt={`${this.props.product.title} product shot`}
+            />
+          ) : null}
         </div>
         <div className="prod-right">
-
-
-        <div className="product-col">
-        <h1 className="prod-title">{this.props.product.title}</h1>
-        <span className="prod-price">${variant.price}</span>
-        <div className="prod-description" dangerouslySetInnerHTML={this.props.product.descriptionHtml.value}>{this.props.product.description}</div>
-        {variantSelectors}
-        {/* <label className="Product__option">
+          <div className="product-col">
+            <h1 className="prod-title">{this.props.product.title}</h1>
+            <span className="prod-price">${variant.price}</span>
+            <div
+              className="prod-description"
+              dangerouslySetInnerHTML={this.props.product.descriptionHtml.value}
+            >
+              {this.props.product.description}
+            </div>
+            {variantSelectors}
+            {/* <label className="Product__option">
           Quantity */}
-          {/* <div className="quantity-box"> */}
-          <div className="product-row">
-          {/* <span onClick={()=>{this.quantChange('+', variant.id, variantQuantity)}}>+</span> */}
-          <div className="qty-product">QTY</div>
-          <input min="0" type="number" defaultValue={0} value={variantQuantity} onChange={this.handleQuantityChange}></input>
-          {/* <span onClick={()=>{this.quantChange('-', variant.id, variantQuantity)}}>-</span> */}
-        <button className="Product__buy button" onClick={() => this.props.addVariantToCart(variant.id, variantQuantity)}>Add to cart</button>
+            {/* <div className="quantity-box"> */}
+            <div className="product-row">
+              {/* <span onClick={()=>{this.quantChange('+', variant.id, variantQuantity)}}>+</span> */}
+              <div className="qty-product">Qty</div>
+              <input
+                min="0"
+                type="number"
+                defaultValue={0}
+                value={variantQuantity}
+                onChange={this.handleQuantityChange}
+              ></input>
+              {/* <span onClick={()=>{this.quantChange('-', variant.id, variantQuantity)}}>-</span> */}
+              <button
+                className="Product__buy button"
+                onClick={() =>
+                  this.props.addVariantToCart(variant.id, variantQuantity)
+                }
+              >
+                Add to cart
+              </button>
+            </div>
+            {/* </div> */}
           </div>
-        {/* </div> */}
+
+          {/* </label> */}
+          {/* </Link> */}
         </div>
-         
-        {/* </label> */}
-        {/* </Link> */}
-          </div>
       </div>
     );
   }
